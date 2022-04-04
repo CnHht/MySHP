7e488cf892b8775f12b30a2e1bd6663d12af4419
312312 <2713225485@qq.com>
2022-04-04 14:07:52 +0800

    完成p13-Home组件的静态页面

--- app/src/main.js
+++ app/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false
 import router from '../src/router'
+//注册三级联动组件（全局组件）
+import typeNav from '../src/pages/Home/tpyeNav'
+Vue.component(typeNav.name,typeNav)
+
+
 new Vue({
   render: h => h(App),
   //注册路由组件
-  router
+  router,
 }).$mount('#app')
--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>这是Home组件</div>
+  <div>
+    <!--使用三级联动组件，已注册为全局组件，不需要额外引入    -->
+    <type-nav></type-nav>
+    <!--  轮播图与快报组件-->
+    <list-container></list-container>
+    <!--  今天推荐组件-->
+    <today-recommend></today-recommend>
+    <!--  热卖排行组件-->
+    <Rank></Rank>
+    <!--  猜你喜欢组件-->
+    <Like></Like>
+    <!--  楼层喜欢组件-->
+    <Floor></Floor>
+    <Floor></Floor>
+    <!--  商标喜欢组件-->
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+import listContainer from './listContainer'
+import todayRecommend from './todayRecommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
 export default {
-  name: "index"
+  name: "index",
+  components:{
+    listContainer,
+    todayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 }
 </script>
 
