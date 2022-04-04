63d223fd16b2c0eb99c36e07f525c623607db0b0
312312 <2713225485@qq.com>
2022-04-04 13:22:58 +0800

    p9,完成

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,11 +4,16 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 //备份VueRouter的push方法
 const originPush = VueRouter.prototype.push;
+const originReplace = VueRouter.prototype.replace;
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Search from '../pages/Search'
 //重写push方法实现消除多次push报错的问题
+//参数一：跳转的位置
+//参数二：函数调用成功的回调
+//参数三：函数调用失败的回调
+
 VueRouter.prototype.push = function (location,resolve,reject) {
     if(resolve && reject){
         originPush.call(this,location,resolve,reject)
@@ -16,6 +21,13 @@ VueRouter.prototype.push = function (location,resolve,reject) {
         originPush.call(this,location,()=>{},()=>{})
     }
 }
+VueRouter.prototype.replace = ( function (location,resolve,reject) {
+    if(resolve && reject){
+        originReplace.call(this,location,resolve,reject)
+    }else {
+        originReplace.call(this,location,()=>{},()=>{})
+    }
+})
 export default new VueRouter({
     //配置路由
     routes:[
